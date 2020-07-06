0e07d63b83475034cfce66d9aa669cc8dd1a8e3d
Gabriel <lydstyl@gmail.com>
2020-07-06 14:50:18 +0200

    respond writeHead with status code and body

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  console.log('req', req)
+const todos = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+  { id: 3, text: 'Todo Three' },
+]
 
-  res.end()
+const server = http.createServer((req, res) => {
+  res.writeHead(400, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js',
+  })
+
+  //   res.write('<h1>Hello</h1>')
+
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: 'Please add email',
+      datas: null,
+    })
+  )
 })
 
 const PORT = 5000
